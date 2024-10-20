6a6fedc58e87d0ef4110e18e8953c53583db2608
Sabab Asmae <sababasmae@gmail.com>
2024-10-20 21:07:44 +0100

    Database tasks

---
+++ task_4.sql
@@ -0,0 +1,4 @@
+SELECT column_name, data_type, is_nullable, column_default, character_maximum_length
+FROM information_schema.columns
+WHERE table_name = 'books'
+AND table_schema = 'alx_book_store';
